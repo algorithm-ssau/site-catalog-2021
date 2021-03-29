4d67996939656418ccf02a5afdc067542f46da41
ChernikovVictor <victorchernikov252@gmail.com>
2021-03-29 23:48:56 +0300

    Add test model Item, add REST API for Item

--- server.js
+++ server.js
@@ -2,6 +2,8 @@ const express = require('express');
 const mongoose = require('mongoose');
 const bodyParser = require('body-parser')
 
+const items = require('./routes/api/items');
+
 const app = express();
 
 // BodyParser Middleware
@@ -11,17 +13,25 @@ app.use(bodyParser.json());
 const dbURI = require('./config/keys').mongoURI;
 
 // Connect to Mongo
-const MongoClient = require('mongodb').MongoClient;
-const client = new MongoClient(dbURI, { useNewUrlParser: true, useUnifiedTopology: true });
-client.connect()
+mongoose.connect(dbURI, {
+    useCreateIndex:true,
+    useFindAndModify:false,
+    useNewUrlParser: true,
+    useUnifiedTopology: true
+})
     .then(() => console.log('MongoDB connected...'))
     .catch(err => console.log(err));
 
-// Connect to Mongo v.2 (deprecated)
-// mongoose.connect(dbURI)
+// Connect to Mongo v.2
+// const MongoClient = require('mongodb').MongoClient;
+// const client = new MongoClient(dbURI, { useNewUrlParser: true, useUnifiedTopology: true });
+// client.connect()
 //     .then(() => console.log('MongoDB connected...'))
 //     .catch(err => console.log(err));
 
 const port = process.env.PORT || 5000;
 
 app.listen(port, () => console.log(`Server started on port ${port}`));
+
+// Use routes
+app.use('/api/items', items)
